998f61468ca2242bae4ba16d6228fb4352999f01
Nicolás Busso <nicolasbusso@gmail.com>
2024-04-27 19:26:07 -0300

    inicio firebase

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Entrega 2 - Nicolás Busso
+# Entrega Final - Nicolás Busso
+
+**VOY A SEGUIR AGREGANDO COMMITS DESPUÉS DE LA ENTREGA!!**
 
 Como Framework de CSS estoy usando Tailwind CSS + Material-Tailwind
+
+**README EN CONSTRUCCIÓN**
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
 import { ThemeProvider } from "@material-tailwind/react";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtiHWrVxucXk30pX6A3ONRPZad2r9yT4M",
+  authDomain: "busso-ecommerce.firebaseapp.com",
+  projectId: "busso-ecommerce",
+  storageBucket: "busso-ecommerce.appspot.com",
+  messagingSenderId: "1063351134743",
+  appId: "1:1063351134743:web:b772895c20d58d3c5c4a52",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
